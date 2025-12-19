74a8bf816067e568d599f1fd1202064bef3ae223
A先生 <142523398+axs66@users.noreply.github.com>
2025-12-19 15:41:29 +0800

    Delete TrollFools directory

--- TrollFools/Constants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Constants.swift
-//  TrollFools
-//
-//  Created by 82Flex on 3/8/25.
-//
-
-let gTrollFoolsIdentifier = "wiki.qaq.TrollFools"
-let gTrollFoolsErrorDomain = "\(gTrollFoolsIdentifier).error"
